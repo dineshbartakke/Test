d9124147f56f652cd4e30f3e17d9b1b11083fa99
dinesh <dinesh.bartakke@outlook.com>
2016-09-05 19:28:31 -0400

    test

---
+++ mockitoTest/src/main/java/com/test/MathTest.java
@@ -0,0 +1,14 @@
+package com.test;
+
+import com.test.*;
+
+public class MathTest {
+
+	public static void main(String args[]){
+
+		Math mathObj = new Math();
+
+		System.out.println(" Add (2, 3)--> " + mathObj.add(2, 3));		
+		System.out.println(" Add (4, 6)--> " + mathObj.add(4,6));
+	}
+}
